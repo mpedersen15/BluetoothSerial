7d9da752d5914d261b57fc2e2e4872aaadab329e
mpedersen15 <mpedersen@mindrocketdesign.com>
2014-03-18 12:47:34 -0600

    Update BLEDefines.h

--- src/ios/BLEDefines.h
+++ src/ios/BLEDefines.h
@@ -1,4 +1,3 @@
-
 /*
  
  Copyright (c) 2013 RedBearLab
@@ -12,8 +11,12 @@
  */
 
 // RBL Service
-#define RBL_SERVICE_UUID                         "713D0000-503E-4C75-BA94-3148F18D941E"
-#define RBL_CHAR_TX_UUID                         "713D0002-503E-4C75-BA94-3148F18D941E"
-#define RBL_CHAR_RX_UUID                         "713D0003-503E-4C75-BA94-3148F18D941E"
+//#define RBL_SERVICE_UUID                         "713D0000-503E-4C75-BA94-3148F18D941E"
+//#define RBL_CHAR_TX_UUID                         "713D0002-503E-4C75-BA94-3148F18D941E"
+//#define RBL_CHAR_RX_UUID                         "713D0003-503E-4C75-BA94-3148F18D941E"
+
+#define RBL_SERVICE_UUID                         "0xFFE0"
+#define RBL_CHAR_TX_UUID                         "0xFFE1"
+#define RBL_CHAR_RX_UUID                         "0xFFE1"
 
 #define RBL_BLE_FRAMEWORK_VER                    0x0200
